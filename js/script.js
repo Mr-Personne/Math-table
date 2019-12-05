d01cfb865b6429e05fff1caf562efe3df0da2198
Mr-Personne <h.sacha07@gmail.com>
2019-12-05 11:49:01 +0000

    got rid of other function other than ajax call

--- js/script.js
+++ js/script.js
@@ -1,29 +1,5 @@
 var answer;
 
-function revision(num) {
-    var randNum = Math.round(Math.random() * 10);
-
-    var stringQestion = randNum + " X " + num + "?";
-    answer = randNum * num;
-    console.log(stringQestion);
-    ajaxCallAsynch();
-    //timeout so ajax has time to load page
-    // setTimeout(function () { document.querySelector(".calcule").innerText = stringQestion; }, 500);
-}
-
-
-function checkAnswer(calc) {
-    var yourAnswer = document.querySelector(".your-answer").value;
-    var section = document.querySelector(".mode-révision");
-    if (yourAnswer == answer) {
-        section.innerHTML = "Oui! Bravo!";
-    }
-    else{
-        alert("Non! Essaye encore...");
-    }
-    console.log(yourAnswer);
-}
-
 var httpRequest = null;
 
 function ajaxCallAsynch(pageToLoad) {
