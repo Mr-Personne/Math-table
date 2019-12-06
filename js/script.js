717d1a01f06f4064600915011eb3455a366e4224
Mr-Personne <h.sacha07@gmail.com>
2019-12-06 15:44:23 +0000

    added ligne to color correct answers background in supercheck funtion

--- js/script.js
+++ js/script.js
@@ -31,27 +31,27 @@ function ajaxCallAsynchSuper(num) {
         console.log("httpRequest en cours...");
     }
     else {
-        console.log("ajax super 1: ",currentNum," num : ",num);
+        console.log("ajax super 1: ", currentNum, " num : ", num);
         //if user checked multiples numbers to révise, add their values into a string
-        if(document.querySelectorAll('main input[type=checkbox]:checked').length != 0){
+        if (document.querySelectorAll('main input[type=checkbox]:checked').length != 0) {
             var selectedNums = document.querySelectorAll('main input[type=checkbox]:checked');
             num = "";
-            for(var i = 0 ; i < selectedNums.length; i++) {
-                num += selectedNums[i].value+"-";
+            for (var i = 0; i < selectedNums.length; i++) {
+                num += selectedNums[i].value + "-";
             };
         }
         // console.log(document.querySelectorAll('main input[type=checkbox]:checked').length);
         httpRequest = new XMLHttpRequest();
         httpRequest.open("GET", "super-revision.php?currentTable=" + num, true);
         httpRequest.send();
-        console.log("ajax super 2: ",currentNum," num : ",num);
+        console.log("ajax super 2: ", currentNum, " num : ", num);
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState === 4) {
                 currentNum = num;
                 var section = document.querySelector(".mode-révision");
                 var mainSection = document.querySelector("section:first-child");
                 // console.log("httpRequest : ",httpRequest);
-                console.log("ajax super 3: ",currentNum," num : ",num);
+                console.log("ajax super 3: ", currentNum, " num : ", num);
                 mainSection.innerHTML = httpRequest.responseText;
                 httpRequest = null;
             }
@@ -65,10 +65,10 @@ function checkAnswer(answer) {
     var section = document.querySelector(".mode-révision");
     // console.log("yourAnswer : ",yourAnswer, " answer : ",answer);
     if (yourAnswer == answer) {
-        section.innerHTML = '<p>Oui! Bravo!</p><button type="button" class="revision" onclick="ajaxCallAsynch(' + currentNum + ')">Re-teste moi!</button>';
+        section.innerHTML = '<p>Oui! Bravo!</p><button type="button" class="revision basic-button" onclick="ajaxCallAsynch(' + currentNum + ')">Re-teste moi!</button>';
         // setTimeout(function () { section.innerHTML = "<p></p>";revision(currentNum); }, 1000);
     }
-    else{
+    else {
         section.innerHTML = "<p>Non! Essaye encore...</p>";
         setTimeout(function () { section.innerHTML = "<p></p>"; }, 2000);
     }
@@ -81,16 +81,18 @@ function checkMultipleAnswers(answer) {
     var correctAnswers = answer.split(",");
     var score = 0;
     // console.log("yourAnswer : ", yourAnswer, " answer : ", answer);
-    for (var i=0; i < correctAnswers.length-1 ; i++){
+    for (var i = 0; i < correctAnswers.length - 1; i++) {
         if (yourAnswer[i].value == correctAnswers[i]) {
             score += 1;
-            
+            console.log(yourAnswer);
+            yourAnswer[i].style.backgroundColor = "#8aa751";
+
             // setTimeout(function () { section.innerHTML = "<p></p>";revision(currentNum); }, 1000);
         }
         // console.log(correctAnswers[i]);
     }
     console.log("check5 : ", currentNum);
-    section.innerHTML = '<p>Votre score est de : '+ score +'/5</p><button type="button" class="revision" onclick="ajaxCallAsynchSuper(' +"'"+ currentNum+"'" + ')">Re-teste moi!</button>';
-    
+    section.innerHTML = '<p>Votre score est de : ' + score + '/5</p><button type="button" class="revision basic-button" onclick="ajaxCallAsynchSuper(' + "'" + currentNum + "'" + ')">Re-teste moi!</button>';
+
 };
 
